2ae1d30fa6e8a55bcb317951ef719e911e64bb3a
Edoardo Grassi <edo.g@hotmail.it>
2020-01-31 16:38:54 +0100

    Added tests, custom exception and auto-login on token expiration

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,100 @@
+import unittest
+from unittest.mock import patch
 
-def test_main(settings, client):
-    assert 3 == 3
+from mailupy import Mailupy
+from .tools import mock_request, mock_request_refresh_token
+
+
+class TestClient(unittest.TestCase):
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_get_fields(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.get_fields()['Items'][0]['Id'] == 27
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_get_groups_from_list(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.get_groups_from_list(1)['Items'][0]['idGroup'] == 6
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_get_users_from_list(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.get_users_from_list(1)['Items'][0]['idRecipient'] == 13
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_get_users_from_group(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.get_users_from_group(6)['Items'] == []
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_get_message_by_subject(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.get_message_by_subject(1, 'QWERTYUIOP')['Items'][0]['Subject'] == 'QWERTYUIOP'
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_get_message_by_tags(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.get_message_by_tags(1, '')['Items'][0]['Subject'] == 'QWERTYUIOP'
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_send_message(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.send_message('email@email.email', 1)
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_create_group(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.create_group(1, 'TEST')['idGroup'] == 9
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_subscribe_to_list(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.subscribe_to_list(1, 'ASDFGHJKL', 'email@email.email') == 16
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_subscribe_to_group(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.subscribe_to_group(6, 'ASDFGHJKL', 'email@email.email', {'test': 'test'}) == 18
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_update_customer_field(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.update_customer_field('ASDFGHJKL', 'email+1@email.email', {'test': 'test1'})['idRecipient'] == 18
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_unsubscribe_from_list(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.unsubscribe_from_list(1, 18)
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_unsubscribe_from_group(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.unsubscribe_from_group(6, 18)
+
+    @patch('mailupy.Mailupy._requests_wrapper', side_effect=mock_request)
+    def test_remove_from_list(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+
+        assert m.remove_from_list(1, 18)
+
+    @patch('requests.api.request', side_effect=mock_request_refresh_token)
+    def test_refresh_token(self, func):
+        m = Mailupy('username', 'password', 'client-id', 'client-secret')
+        m._token = 'bad_token'
+
+        m.get_fields()
+        assert m._token == 'good_token'
