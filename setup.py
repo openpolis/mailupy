ada89715e5be12c0619be0f90b9025fcf8d5217c
Guglielmo Celata <97271+guglielmo@users.noreply.github.com>
2021-12-31 12:38:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.1.0',
     url='https://github.com/lotrekagency/mailupy',
     install_requires=[
-        "requests==2.24.0"
+        "requests==2.26.0"
     ],
     description="Yet another Mailup Python client",
     long_description=open('README.rst', 'r').read(),
